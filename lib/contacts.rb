fa87cccff72b3f16461954a37a449dedbcec3aa8
Ryan Zemlin <rzemlin@gmail.com>
2021-02-25 16:07:32 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attritbute, data|
+        binding.pry
   end
 end
